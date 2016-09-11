699d34648cf07e49f518e4ccd5e79fe7cad67e09
Ulrika Malmgren <ulrika.malmgren@magine.com>
2016-09-11 18:56:34 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Arbetsprov::Application.config.secret_token = '63ed00395ab4ee058724f474a8494f0b620c29a5c49cc207919b377f9fbe9ddd9cb71db481677cf34910c14ada5f4792b777e19f4f9e471a012fac3f1db834a0'
