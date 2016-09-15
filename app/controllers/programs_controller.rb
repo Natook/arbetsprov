79a79e5357cb2f872859903c45c093e104431bbe
Ulrika Malmgren <ulrika.malmgren@magine.com>
2016-09-15 20:45:01 +0200

    Listing participants for Letsdance

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -1,6 +1,15 @@
-class ProgramsController < ApplicationController
-  def home
-		
-  end 
+require 'net/http'
+require 'uri'
+require 'json'
 
+class ProgramsController < ApplicationController
+
+  def home
+		uri = URI.parse("http://api.tv4play.se/site/programs/lets-dance")
+		response = JSON.parse(Net::HTTP.get(uri))
+		
+		@programs = response['participant_groups'][0]['participants']
+
+
+  end 
 end
